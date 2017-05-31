14af9aa0499de475ff17cb7b2b0019876163a4eb
einarsandberg <einars92@gmail.com>
2017-05-31 09:52:18 +0200

    fixed some styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+//const PieChart = require('react-d3/piechart').PieChart;
 const PieChart = require('rd3/build/cjs/piechart').PieChart;
 import EntryForm from './EntryForm'
 import FormInput from './FormInput'
@@ -29,26 +30,34 @@ export default class App extends Component {
 
     onSubmitEntry = () => {
      this.setState({
-        pieData: this.calculatePercent(this.state.pieData, this.newEntry).sort(this.compare)
+        pieData: this.recalculate(this.state.pieData, this.newEntry).sort(this.compare)
       })
     }
-    calculatePercent = (data, newEntry) => {
+
+    recalculate = (data, newEntry) => {
+
       // Remove all other items if new entry is >= 100%
-      if (newEntry.value >= 100) {
+      if (newEntry.value >= 100)
         return [{label: newEntry.label, value: 100}]
-      }
-      const sum = data.reduce((acc, item) => acc + item.value, newEntry.value)
-      // If sum > 100, calculate new values for old entries.
-      // The new entry will be the input value, i.e no recalculation
-      if (sum > 100) {
-        data.map((item) => {
-          item.value *= (0.01*(100-newEntry.value))
-          // If not integer -> fixed num of decimals
-          if (item.value % 1 != 0)
-            item.value = parseFloat(item.value.toFixed(2))
-          return item;
-        })
-      }
+      data = data.filter(function(a) {
+        return a.label != newEntry.label
+      })
+      let sum = data.reduce((acc, item) => acc + item.value, 0)
+      console.log(sum)
+      // Recalculate existing items. Will not change unless item is
+      data = data.map((item) => {
+        item.value = (item.value/sum)*100
+        return item
+      })
+      console.log(data)
+      data = data.map((item) => {
+        item.value *= (0.01*(100-newEntry.value))
+        // If not integer -> fixed num of decimals
+        if (item.value % 1 != 0)
+          item.value = parseFloat(item.value.toFixed(2))
+        return item;
+      })
+      
       data.push({'label': newEntry.label, 'value': newEntry.value})
       return data
     }
@@ -62,23 +71,35 @@ export default class App extends Component {
             <h1>Hello PieChart!</h1>
             <div id="container">
               <div id="pie-container">
-                <PieChart
-                  data={this.state.pieData}
-                  width={this.chartSize.width}
-                  height={this.chartSize.height}
-                  radius={this.chartSize.radius}
-                  innerRadius={this.chartSize.innerRadius}
-                  colors = {d3.scale.category10()}
-                  showOuterLabels={true}
-                  showInnerLabels={true}
-                  sectorBorderColor={'black'}
-                />
-              <div id="entry-form">
-                <EntryForm onSubmit={this.onSubmitEntry} heading="Add new entry" submitBtnText="Add new pie entry" >
-                  <FormInput placeholder="Jane" type="text" id="label" name="Entry " onChange={this.onInputUpdate}/>
-                  <FormInput placeholder="20"type="text" id="value" name="Value " onChange={this.onInputUpdate} ensure="number"/>
-                </EntryForm>
-              </div>
+                <div id="pie">
+                  <PieChart
+                    data={this.state.pieData}
+                    width={this.chartSize.width}
+                    height={this.chartSize.height}
+                    radius={this.chartSize.radius}
+                    innerRadius={this.chartSize.innerRadius}
+                    colors = {d3.scale.category10()}
+                    showOuterLabels={true}
+                    showInnerLabels={true}
+                    sectorBorderColor={'black'}
+                  />
+                </div>
+                <div id="entry-form">
+                  <EntryForm onSubmit={this.onSubmitEntry} heading="Add or update entry" submitBtnText="Submit" >
+                    <FormInput placeholder="Jane" 
+                      type="text" id="label" 
+                      name="Entry " 
+                      onChange={this.onInputUpdate}
+                    />
+                    <FormInput placeholder="20" 
+                      type="text" id="value" 
+                      name="Value " 
+                      onChange={this.onInputUpdate} 
+                      ensure="number"
+                    />
+                  </EntryForm>
+                </div>
+
             </div>
           </div>
 
